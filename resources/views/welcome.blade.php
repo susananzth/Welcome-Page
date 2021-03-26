7277b9c762b328adeb3d7d2da97ca838329a38ef
Susana Piñero <susananzth@gmail.com>
2021-03-25 21:53:40 -0500

    Maketando footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
         <!-- Título de la página -->
         <title>Welcome - Pague</title>
         <!-- Ícono -->
-        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.png')}}" />
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico')}}" />
         <!-- Fuentes -->
         <script data-search-pseudo-elements defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.24.1/feather.min.js" crossorigin="anonymous"></script>
@@ -30,8 +30,99 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
         <link rel="stylesheet" href="{{ asset('css/myStyles.css') }}">
     </head>
-    <body class="antialiased">
- 
+    <body>
+        <div id="layout">
+            <div id="layoutContent">
+                <main>
+                    <nav class="navbar fixed-top">
+                    </nav>
+                    <header>
+
+                    </header>
+                    <section id="section-1">
+                        <div class="container">
+                        </div>
+                    </section>
+                    <hr/>
+                    <section id="section-2">
+                        <div class="container">
+                        </div>
+                    </section>
+                    <section id="section-3">
+                        <div class="container">
+                        </div>
+                    </section>
+                    <section id="section-4">
+                        <div class="container">
+                        </div>
+                    </section>
+                    <section id="section-5">
+                        <div class="container">
+                        </div>
+                    </section>
+                    <hr/>
+                </main>
+            </div>
+            <!-- layoutFooter -->
+            <div id="layoutFooter">
+                <footer class="footer">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-3">
+                                <div class="footer-brand">Welcome Page</div>
+                                <div class="mb-3">Multipurpose Welcome Website. With Bootstrap 5 and Laravel 8.12.</div>
+                                <div>
+                                    <a target="_blank" href="https://twitter.com/susananzth"><i class="fab fa-twitter"></i></a>
+                                    <a target="_blank" href="https://facebook.com/susananzth"><i class="fab fa-facebook-f"></i></a>
+                                    <a target="_blank" href="https://www.linkedin.com/in/susananzth/"><i class="fab fa-linkedin"></i></a>
+                                    <a target="_blank" href="https://github.com/susananzth"><i class="fab fa-github"></i></a>
+                                    <a target="_blank" href="https://gitlab.com/susananzth"><i class="fab fa-gitlab"></i></a>
+                                </div>
+                            </div>
+                            <div class="col-lg-9">
+                                <div class="row">
+                                    <div class="col-lg-4">
+                                        <div>Landings</div>
+                                        <ul class="list-unstyled">
+                                            <li class="mb-2"><a class="link-primary" href="javascript:void(0);">Multipurpose with Vue JS</a></li>
+                                            <li class="mb-2"><a href="javascript:void(0);">Multipurpose with Bootstrap 5</a></li>
+                                            <li class="mb-2"><a href="javascript:void(0);">Portfolio with Vue JS</a></li>
+                                            <li class="mb-2"><a href="javascript:void(0);">Portfolio with Vue JS + API</a></li>
+                                            <li><a href="javascript:void(0);">Portfolio with Bootstrap 5</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-lg-4">
+                                        <div>E-commerce</div>
+                                        <ul class="list-unstyled">
+                                            <li class="mb-2"><a href="javascript:void(0);">E-commerce - Laravel </a></li>
+                                            <li class="mb-2"><a href="javascript:void(0);">E-commerce - Node JS </a></li>
+                                            <li><a href="javascript:void(0);">UI Kit E-commerce - Bootstrap</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-lg-4">
+                                        <div>Other</div>
+                                        <ul class="list-unstyled">
+                                            <li class="mb-2"><a href="javascript:void(0);">Ticket Support - Laravel</a></li>
+                                            <li class="mb-2"><a href="javascript:void(0);">Player - Vue JS</a></li>
+                                            <li><a href="javascript:void(0);">Updates</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <hr class="my-5" />
+                        <div class="row align-items-center">
+                            <div class="col-md-6">Copyright &copy; Susana Piñero Rodríguez 2021</div>
+                            <div class="col-md-6 text-end">
+                                <a href="javascript:void(0);">Privacy Policy</a>
+                                &middot;
+                                <a href="javascript:void(0);">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div> <!-- layoutFooter -->
+        </div>
         <!-- JS Bootstrap -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     </body>
